f62a61c1e86b9838f31a93798aa166283b6aa2e5
izawasoma <izawasoma@gmail.com>
2022-01-23 09:48:04 +0900

    itiou

---
+++ sns_calendar/function/date_function.php
@@ -0,0 +1,51 @@
+<?php
+
+function get_date_list($every,$frequency,$end,$start = ""){
+    //$startに値が渡されていない場合、本日の日付を代入
+    if($start == ""){
+        $start = date('Y-m-d');
+    }
+
+    //結果を代入する配列
+    $result = [];
+
+    //dateオブジェクト、end_pointオブジェクトを作成
+    $date = new DateTime($start);
+    $end_point = new DateTime($end);
+    $end_point = $end_point->modify('+ 1day');
+
+    //以降$everyによって処理が分かれる
+    if($every == 'days'){
+        while($date < $end_point){
+            $result[] = $date->format('Y-m-d');
+            $date->modify('+' . $frequency . ' day')->format('Y-m-d');
+        }
+    }
+    elseif($every == 'weeks'){
+        while($date < $end_point){
+            $result[] = $date->format('Y-m-d');
+            $date->modify('+' . ($frequency * 7) . ' day')->format('Y-m-d');
+        }
+    }
+    elseif($every == 'months'){
+        while($date < $end_point){
+            $result[] = $date->format('Y-m-d');
+            $date->modify('+' . $frequency . 'month')->format('Y-m-d');
+        }
+    }
+    elseif($every == 'years'){
+        while($date < $end_point){
+            $result[] = $date->format('Y-m-d');
+            $date->modify('+' . $frequency . 'years')->format('Y-m-d');
+        }
+    }
+    elseif($every == 'no'){
+        $result[] = $date->format('Y-m-d');
+    }
+    else{
+        $result = false;
+    }
+    return $result;
+}
+
+?>
--- sns_calendar/group.php
+++ sns_calendar/group.php
@@ -3,6 +3,10 @@ require_once './initial_setting.php';
 
 $add_schedule_modal = 'none';
 
+if(isset($_POST['room_id'])){
+    $_GET['room_id'] = $_POST['room_id'];
+}
+
 //======================================
 //●不正アクセスを防ぐ処理
 //======================================
@@ -41,5 +45,73 @@ else{
         exit;
     }
 }
+
+//======================================
+//●ブロック追加処理
+//======================================
+if(isset($_POST['button']) && $_POST['button'] == "add_schedule"){
+    //================================
+    //●エラーチェック
+    //================================
+
+    //エラー文を格納する連想配列
+    $error = [];
+
+    //(未入力)
+    $error['title'] = not_entered_check($_POST['title']);
+    $error['start'] = not_entered_check($_POST['start']);
+    $error['end'] = not_entered_check($_POST['end']);
+    if($_POST["repeat_every"] != "no"){
+        $error['end_repeat'] = not_entered_check($_POST['end_repeat']);
+        $today = new DateTime();
+        $end_repeat = new DateTime($_POST['end_repeat']);
+        if($today > $end_repeat){
+            $error["end_repeat"] = DATE_INVALID;
+        }
+    }
+    //日付の妥当性チェック
+    if($_POST["start"] > $_POST["end"]){
+        $error["start"] = TIME_INVALID;
+        $error["end"] = TIME_INVALID;
+    }
+
+    //エラーがない場合
+    if(error_count($error) == 0){
+        //=================================
+        //●テンプレートに追加
+        //=================================
+
+        //DB接続
+        $link = mysqli_connect(HOST,USER_ID,PASSWORD,DB_NAME);
+        //クエリを作成
+        $title = escape($link,$_POST["title"]);
+        $column = ['group_id','title','start','end','repeat_every','repeat_frequency','end_repeat','created_at'];
+        $data = [$_GET['room_id'],$title,$_POST['start'],$_POST['end'],$_POST['repeat_every'],$_POST['repeat_frequency'],$_POST['end_repeat'],date("Y-m-d")];
+        $sql = create_insert_sql('block_template',$column,$data);
+        //DBを実行
+        $id = db_run_insert($link,$sql);
+
+        //=================================
+        //●テンプレートからブロックを追加
+        //=================================
+        $date_list = get_date_list($_POST['repeat_every'],$_POST['repeat_frequency'],$_POST['end_repeat']);
+
+        //DB接続
+        $link = mysqli_connect(HOST,USER_ID,PASSWORD,DB_NAME);
+        //クエリを作成
+        $column = ['group_id','block_template_id','title','start','end'];
+        $data = [];
+        foreach($date_list as $value){
+            $data[] = [$_GET['room_id'],$id,$title,$value . ' ' . $_POST['start'],$value . ' ' . $_POST['end']];
+        }
+        $sql = create_insert_sql('block',$column,$data);
+        //DBを実行
+        $id = db_run_insert($link,$sql);
+
+    }else{
+        $add_schedule_modal = 'display';
+    }
+}
+
 require_once './tpl/'.basename(__FILE__);
 ?>
